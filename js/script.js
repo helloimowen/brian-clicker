70f43413934872d8d4f59bada2832948ff2ee0e6
owen <helloimowen>
2018-02-14 16:48:13 -0500

    hot garbage coming at you.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,62 @@ var frameCount = 0; // globals are good.
 var element = document.getElementById("papersGraded");
 var element2 = document.getElementById("papersToGrade");
 var element3 = document.getElementById("dying");
+var seasonDom = document.getElementById("dying");
+
+//import seasons from 'seasons.js'; // or './module'
+//let seasons = seasons(); // val is "Hello";
+
+var seasonsENUM = {"fall":1, "winter":2, "spring":3, "summer":4};
+
+const days = 365; 
+var seasonsENUM = {"fall":1, "winter":2, "spring":3, "summer":4};
+var currentSeason; 
+//fall semester = first 100 days. 
+//winter break = 65 days. 
+    //sort of like stardew valley. You can't do much. 
+//spring semester = 100 days. 
+    // new students. More 
+//summer break = 105 days. 
+    //brian begins making BBQ sauce. grading is replaced with barbeque sauce making. 
+    //Students work on making barbeque sauce. Students fall in vats of barbeque sauce. 
+
+    
+//class seasons
+//{
+    //constructor(){
+        //days = 365;
+       // currentSeason = seasonsENUM.fall;
+   // }
+
+
+
+    checkSeason(frame){
+
+        frame = frame % days; 
+
+        frame /= 90; 
+
+        if (frame <= 100) // increment a day every three seconds. 
+        {       // This changes the season around... every five minutes. 
+            currentSeason = seasonsENUM.fall;
+        }
+        else if  (frame > 100 && frame <= 165)
+        {
+            currentSeason = seasonsENUM.winter;
+        }
+        else if (frame > 165 && frame <= 265)
+        {
+            currentSeason = seasonsENUM.spring;
+        }
+        else
+        {
+            currentSeason = seasonsENUM.summer;
+        }
+
+        return 
+    } 
+//}
+
 
 function grade()
 {
@@ -14,13 +70,13 @@ function grade()
     if (papersGraded == 0 && papersToGrade > 0)
     {
         papersGraded++; 
-        element.innerHTML = "Brian has graded " + papersGraded + " assignmet.";
+        setDOM(element, "Brian has graded " + papersGraded + " assignmet.");
         papersToGrade--; 
     }
     else if (papersToGrade > 0)
     {
         papersGraded++; 
-        element.innerHTML = "Brian has graded " + papersGraded + " assignmets.";
+        setDOM(element, "Brian has graded " + papersGraded + " assignmets.");
         papersToGrade--; 
     }
 
@@ -46,6 +102,10 @@ function loop()
             calculatePapers();
         }
 
+        setDOM( seasonDom, "It is currently the" + seasons.checkSeasons() );
+
+
+
 
     }, (1000 / 30));
 }
@@ -55,11 +115,13 @@ function calculatePapers()
 
      if (papersToGrade == 1)
         {
-            element2.innerHTML = "Brian has " +  papersToGrade + " assignmet to grade.";
+            //element2.innerHTML = "Brian has " +  papersToGrade + " assignmet to grade.";
+            setDOM(element2, ("Brian has " +  papersToGrade + " assignmet to grade."));
         }
-        else
+    else
         { 
-            element2.innerHTML = "Brian has " +  papersToGrade + " assignmets to grade.";
+            //element2.innerHTML = "Brian has " +  papersToGrade + " assignmets to grade.";
+            setDOM(element2, "Brian has " +  papersToGrade + " assignmets to grade.");
         }
 
 }
@@ -68,6 +130,13 @@ function dying()
 {
     if (papersGraded > 20)
     {
-        element3.innerHTML = "Bryan doesn't want to grade anymore papers. But he can't hire anyone because this game is not finished.";
+        setDOM(element3, "Bryan doesn't want to grade anymore papers. But he can't hire anyone because this game is not finished.")
     }
 }
+
+
+function setDOM(element, string) // pass an item in the DOM some text. 
+{
+    element.innerHTML = string; 
+}
+
